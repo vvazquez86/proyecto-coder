4b22870f8cd05f65c52886d5f6d91f357a299217
vvazquez86 <v.h.vazquez@live.com.ar>
2022-12-25 21:08:53 -0300

    Primera pre entrega v3

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
     return (        
 
         <nav className="bg-body-tertiary">
-            <div className="d-flex justify-content-between aling-item-center">                
+            <div className="d-flex justify-content-between align-items-center">                
                 <div>
                 <img style={{width:'120px', height:'40px'}} src="./img/logo.png" alt="carrito" />
                 </div>
-                <div className="d-flex align-items-center justify-content-center">
+                <div className="">
                     <Boton label='Home' handleClick={()=>console.log('Home')}/>
                     <Boton label='Productos' handleClick={()=>console.log('Productos')}/>
                     <Boton label='Contacto' handleClick={()=>console.log('Contacto')}/>                    
